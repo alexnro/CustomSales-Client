b17d2f8b5bfbae745ff5a24bada6751c4c334731
alexnro <alexjandron99@gmail.com>
2020-05-01 18:54:38 +0200

    feat: Add app bar with drawer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppBarComponent from './components/AppBarComponent';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-
+                <AppBarComponent />
             </header>
         </div>
     );

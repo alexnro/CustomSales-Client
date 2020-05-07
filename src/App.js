f21eb1858cea32c0223bd97880c96859dbc99b55
alexnro <alexjandron99@gmail.com>
2020-05-07 18:46:40 +0200

    refactor: divide gridmenu style in other file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import AppBarComponent from './components/Header/AppBarComponent';
-import GridMenuComponent from './components/GridMenuComponent';
+import GridMenuComponent from './components/GridMenu/GridMenuComponent';
 
 function App() {
     return (
--- src/components/GridMenu/GridMenuComponent.js
+++ src/components/GridMenu/GridMenuComponent.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import useStyles from './GridMenuStyle';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-}));
 
 export default function GridMenuComponent() {
     const classes = useStyles();

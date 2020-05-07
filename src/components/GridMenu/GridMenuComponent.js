5f2ac3d329bcd93b413e169967e19964b9dd9f02
alexnro <alexjandron99@gmail.com>
2020-05-07 20:37:14 +0200

    feat: temporal style for grid menu

--- src/components/GridMenu/GridMenuComponent.js
+++ src/components/GridMenu/GridMenuComponent.js
@@ -3,28 +3,25 @@ import useStyles from './GridMenuStyle';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-
 export default function GridMenuComponent() {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <Grid container spacing={1}>
-                <Grid container item xs={12} spacing={3}>
-                    <Grid item xs={4}>
-                        <Paper className={classes.paper}>Nuevo pedido</Paper>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Paper className={classes.paper}>Gestión de pedidos</Paper>
-                    </Grid>
+            <Grid container item xs={12} spacing={3}>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>Nuevo pedido</Paper>
                 </Grid>
-                <Grid container item xs={12} spacing={3}>
-                    <Grid item xs={4}>
-                        <Paper className={classes.paper}>Productos</Paper>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Paper className={classes.paper}>Gestión de clientes</Paper>
-                    </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>Gestión de pedidos</Paper>
+                </Grid>
+            </Grid>
+            <Grid container item xs={12} spacing={3}>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>Productos</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>Gestión de clientes</Paper>
                 </Grid>
             </Grid>
         </div>
--- src/components/GridMenu/GridMenuStyle.js
+++ src/components/GridMenu/GridMenuStyle.js
@@ -2,13 +2,16 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        marginTop: "20%",
+        marginLeft: "20%",
         flexGrow: 1,
     },
     paper: {
         padding: theme.spacing(1),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-    },
+        cursor: "pointer"
+    }
 }));
 
 export default useStyles;

9e9c5bd0abdea3c4580e2abd2c08e03d3240d0b9
alexnro <alexjandron99@gmail.com>
2020-05-15 01:20:10 +0200

    feat: axios get request to show all products (non-style)

--- src/containers/NewOrder.js
+++ src/containers/NewOrder.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class NewOrder extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get("https://localhost:44345/api/products")
+            .then(response => {
+                this.setState({ products: response.data });
+                console.log(response);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
     render() {
+        const products = this.state.products.map(product => {
+            return <p key={product.id}>{JSON.stringify(product)}</p>
+        })
+
         return (
             <div>
+                {products}
             </div>
         );
     }

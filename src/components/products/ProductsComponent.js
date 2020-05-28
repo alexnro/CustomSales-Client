1314e1321faebb0b1159c80f85c8a32afc68a6bc
alexnro <alexjandron99@gmail.com>
2020-05-28 18:56:21 +0200

    refactor: Delete unnecessary comment

--- src/components/products/ProductsComponent.js
+++ src/components/products/ProductsComponent.js
@@ -7,26 +7,6 @@ import AddProductToCartInput from './AddProductToCartInput';
 import AddProductButton from './addProduct/AddProductButton';
 
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
-
-
-
 const ProductsComponent = (props) => {
     const classes = useStyles();
 
